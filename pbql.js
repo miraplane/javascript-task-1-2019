3cd642a7d874cf189c9fdc69eb356cf2cc1b66a6
Dima Starkov <dimastark@yandex-team.ru>
2019-10-20 22:45:37 +0500

    Заготовка задачи Телефонная книга Аркадия

---
+++ pbql.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * Телефонная книга
+ */
+const phoneBook = new Map();
+
+/**
+ * Вызывайте эту функцию, если есть синтаксическая ошибка в запросе
+ * @param {number} lineNumber – номер строки с ошибкой
+ * @param {number} charNumber – номер символа, с которого запрос стал ошибочным
+ */
+function syntaxError(lineNumber, charNumber) {
+    throw new Error(`SyntaxError: Unexpected token at ${lineNumber}:${charNumber}`);
+}
+
+/**
+ * Выполнение запроса на языке pbQL
+ * @param {string} query
+ * @returns {string[]} - строки с результатами запроса
+ */
+function run(query) {
+    return [];
+}
+
+module.exports = { phoneBook, run };
